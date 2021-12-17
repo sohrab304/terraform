836934b37486287c93f79daf6b78b7c07d1b9e1b
sohrab304 <96132665+sohrab304@users.noreply.github.com>
2021-12-17 16:55:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-    access_key = "AKIASE3Q7NWFUE4YMVKS"
-    secret_key = "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"
+#    access_key = ""
+#    secret_key = ""
 }
